9ed8bbe5e9178d175cad5947d28d56c54690db29
tmartine <thomas.martinet@utbm.fr>
2022-10-07 15:23:02 +0200

    Finalize pokemon exe

--- 2_ExercicesProgrammationImperative/2_Pokemon/test/Tests.kt
+++ 2_ExercicesProgrammationImperative/2_Pokemon/test/Tests.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 class Test {
     @Test fun testSolution() {
         val sentence = getLongestSentence()
+        Assert.assertTrue("The sentence contains only pokemon names", sentence.split(" ").all { it in pokemonNames })
         Assert.assertTrue("Test si chaque mot commence bien par la lettre de fin du mot précédent", sentence.split(" ").zipWithNext().all { it.first.last().uppercase() == it.second.first().uppercase() })
         Assert.assertTrue("Test si la fonction renvoie un résultat sans deux fois le même pokemon", sentence.split(" ").toSet().size == sentence.split(" ").size)
         Assert.assertTrue("Test si la fonction renvoie un résultat avec au moins 10 pokemons", sentence.split(" ").size >= 10)
@@ -11,5 +12,6 @@ class Test {
         Assert.assertTrue("Test si la fonction renvoie un résultat avec au moins 30 pokemons", sentence.split(" ").size >= 30)
         Assert.assertTrue("Test si la fonction renvoie un résultat avec au moins 40 pokemons", sentence.split(" ").size >= 40)
         Assert.assertTrue("Test si la fonction renvoie un résultat avec au moins 50 pokemons", sentence.split(" ").size >= 50)
+
     }
 }
